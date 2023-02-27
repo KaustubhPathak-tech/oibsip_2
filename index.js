d22db577cc6509a2de6282ab6e6a1bcb07a8f9cc
Kaustubh Pathak <kaustubhpathak9@gmail.com>
2023-02-27 23:21:40 +0530

    security update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //jshint esversion: 6
-
+const dotenv = require("dotenv");
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
@@ -7,31 +7,31 @@ const https = require("https");
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-
+dotenv.config();
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function (req, res) {
   const fname = req.body.first;
-  const sub=req.body.subject;
+  const sub = req.body.subject;
   const e_mail = req.body.mail;
   // const lname = req.body.message;
-  
+
   const data = {
     members: [
       {
         email_address: e_mail,
         status: "subscribed",
-        merge_fields: { FNAME: fname, LNAME:sub},
+        merge_fields: { FNAME: fname, LNAME: sub },
       },
     ],
   };
   const jsonData = JSON.stringify(data);
-  const url = "https://us8.api.mailchimp.com/3.0/lists/84c59b071b";
+  const url = process.env.url;
   const options = {
     method: "POST",
-    auth: "kaustubh:5679755d1dded537ab3d10e1ac6ec30c-us8",
+    auth: process.env.auth,
   };
   const request = https.request(url, options, function (response) {
     if (response.statusCode === 200) {
@@ -49,16 +49,9 @@ app.post("/", function (req, res) {
   request.end();
 });
 
-
-app.post("/failure",function(req,res){
-    res.redirect("/");
-})
-app.listen(process.env.PORT||3000, function () {
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+app.listen(process.env.PORT || 3000, function () {
   console.log("server is running on port : 3000");
 });
-
-//api key
-// b474a8d7276aad0bc31ff93588fc6511-us8
-
-// list id
-// 84c59b071b
