1e68ca174bc964a6979ab288ab4e6d80c67c819b
Kaustubh Pathak <kaustubhpathak9@gmail.com>
2023-02-28 00:33:12 +0530

    js update

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 //jshint esversion: 6
 const dotenv = require("dotenv");
 const express = require("express");
+dotenv.config();
 const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-dotenv.config();
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
