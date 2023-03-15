fa59e5e3d0b4d701f33a0b86d1780f4d5d2dca8a
Kaustubh Pathak <kaustubhpathak9@gmail.com>
2023-03-15 15:44:17 +0530

    dsaa

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
 const app = express();
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static(__dirname + "/public/"))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
